7a16788661680c9fa27b47a78da505efc0351dec
Konstantin Lepeshenkov <5447190+scale-tone@users.noreply.github.com>
2022-01-25 23:41:40 +0100

    more tests

--- durablefunctionsmonitor-vscodeext/src/test/suite/MonitorView.test.ts
+++ durablefunctionsmonitor-vscodeext/src/test/suite/MonitorView.test.ts
@@ -5,6 +5,7 @@ import * as os from 'os';
 import * as fs from 'fs';
 import * as path from 'path';
 import * as assert from 'assert';
+import * as vscode from 'vscode';
 
 import { MonitorView } from '../../MonitorView';
 import { Settings } from "../../Settings";
@@ -46,6 +47,37 @@ suite('MonitorView Test Suite', () => {
 		// Assert
 
 		assert.strictEqual(monitorView.isVisible, true);
+
+		const webViewPanel: vscode.WebviewPanel = (monitorView as any)._webViewPanel;
+
+		// Checking links
+		const html = webViewPanel.webview.html;
+
+		const linkToManifestJson = webViewPanel.webview.asWebviewUri(vscode.Uri.file(path.join(backend.binariesFolder, 'DfmStatics', 'manifest.json')));
+		assert.strictEqual(html.includes(`href="${linkToManifestJson}"`), true);
+
+		const linkToFavicon = webViewPanel.webview.asWebviewUri(vscode.Uri.file(path.join(backend.binariesFolder, 'DfmStatics', 'favicon.png')));
+		assert.strictEqual(html.includes(`href="${linkToFavicon}"`), true);
+
+		const cssFolder = path.join(backend.binariesFolder, 'DfmStatics', 'static', 'css');
+		for (const fileName of await fs.promises.readdir(cssFolder)) {
+
+			if (path.extname(fileName).toLowerCase() === '.css') {
+
+				const linkToCss = webViewPanel.webview.asWebviewUri(vscode.Uri.file(path.join(cssFolder, fileName)));
+				assert.strictEqual(html.includes(`href="${linkToCss}"`), true);
+			}
+		}
+
+		const jsFolder = path.join(backend.binariesFolder, 'DfmStatics', 'static', 'js');
+		for (const fileName of await fs.promises.readdir(jsFolder)) {
+
+			if ( !fileName.startsWith('runtime-main.') && path.extname(fileName).toLowerCase() === '.js') {
+
+				const linkToJs = webViewPanel.webview.asWebviewUri(vscode.Uri.file(path.join(jsFolder, fileName)));
+				assert.strictEqual(html.includes(`src="${linkToJs}"`), true);
+			}
+		}
 
 		monitorView.cleanup();
 
