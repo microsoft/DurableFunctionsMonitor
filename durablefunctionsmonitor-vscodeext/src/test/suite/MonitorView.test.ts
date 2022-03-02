e75e35c86b7502e01d9a70e79bdf3d79bd2b7f9c
Konstantin Lepeshenkov <5447190+scale-tone@users.noreply.github.com>
2022-03-02 16:17:27 +0100

    minor test fix

--- durablefunctionsmonitor-vscodeext/src/test/suite/MonitorView.test.ts
+++ durablefunctionsmonitor-vscodeext/src/test/suite/MonitorView.test.ts
@@ -77,10 +77,9 @@ suite('MonitorView Test Suite', () => {
 		const stateFromVsCodeScript = `<script>var OrchestrationIdFromVsCode="",StateFromVsCode=${JSON.stringify(webViewState)}</script>`;
 		assert.strictEqual(html.includes(stateFromVsCodeScript), true);
 
-//		const dfmClientConfigScript = `<script>var DfmClientConfig={'theme':'light','showTimeAs':'UTC'}</script>`;
-//		assert.strictEqual(html.includes(dfmClientConfigScript), true);
-		console.log(html);
-
+		const dfmClientConfigScriptIncluded = html.match(`<script>var DfmClientConfig={'theme':'[^']+','showTimeAs':'UTC'}</script>`);
+		assert.strictEqual(!!dfmClientConfigScriptIncluded, true);
+		
 		const dfmViewModeScript = `<script>var DfmViewMode=0</script>`;
 		assert.strictEqual(html.includes(dfmViewModeScript), true);
 
