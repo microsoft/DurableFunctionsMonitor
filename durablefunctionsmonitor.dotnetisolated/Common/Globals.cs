97ec0bfe2e020cec0579db60c4be53fddd62dd6a
Konstantin Lepeshenkov <5447190+scale-tone@users.noreply.github.com>
2023-08-05 18:35:30 +0200

    minor fixes

--- durablefunctionsmonitor.dotnetisolated/Common/Globals.cs
+++ durablefunctionsmonitor.dotnetisolated/Common/Globals.cs
@@ -10,7 +10,7 @@ using Newtonsoft.Json;
 using Newtonsoft.Json.Converters;
 using Newtonsoft.Json.Serialization;
 
-[assembly: InternalsVisibleToAttribute("durablefunctionsmonitor.dotnetbackend.tests")]
+[assembly: InternalsVisibleToAttribute("durablefunctionsmonitor.dotnetisolated.tests")]
 
 namespace DurableFunctionsMonitor.DotNetIsolated
 {
